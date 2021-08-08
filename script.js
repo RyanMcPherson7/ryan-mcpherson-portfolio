382339632995b14457c5e6253f7f6a0e63dcc994
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2021-08-08 15:41:11 -0400

    switched colors and adjusted navbar bgc changing feature

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // when scroll down, convert nav background from transparent to solid
 window.addEventListener("scroll", () => {
     // if at top of page
-    if (window.pageYOffset == 0) {
+    if (window.pageYOffset <= 0) {
         document.querySelector("#nav-container").style.backgroundColor = "transparent";
     }
     else {
