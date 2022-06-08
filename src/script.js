bbdcb23274c984941ab6b8145a37000a0f20d5f6
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2022-06-08 13:21:14 -0400

    Enable recaptcha on form submit

--- src/script.js
+++ src/script.js
@@ -9,60 +9,59 @@ const colorBG = [
   ['#c884ff', 'bg5.jpg'],
   ['#ffe862', 'bg6.jpg'],
   // ['#5BFDCE', 'bg7.jpg'],
-];
+]
 
-const randomIndex = Math.floor(Math.random() * colorBG.length);
+const randomIndex = Math.floor(Math.random() * colorBG.length)
 
 document.documentElement.style.setProperty(
   '--color-accent',
   colorBG[randomIndex][0]
-);
+)
 document.documentElement.style.setProperty(
   '--header-img',
   `url(../assets/header-images/${colorBG[randomIndex][1]})`
-);
+)
 
 // ====================================================
 // initial header loading animation
 // ====================================================
-const pageLoadCover = document.querySelector('#page-load-cover');
-const pageLoadName = document.querySelector('#page-load-name');
-const pageLoadNameCover = document.querySelector('#page-load-name-cover');
+const pageLoadCover = document.querySelector('#page-load-cover')
+const pageLoadName = document.querySelector('#page-load-name')
+const pageLoadNameCover = document.querySelector('#page-load-name-cover')
 
-const headerMain = document.querySelector('header h1');
-const headerParagraph = document.querySelector('header p');
-const headerResumeButton = document.querySelector('#header-resume-button');
-const navigation = document.querySelector('nav');
-const scrollDownButton = document.querySelector('#scroll-down-button');
+const headerMain = document.querySelector('header h1')
+const headerParagraph = document.querySelector('header p')
+const headerResumeButton = document.querySelector('#header-resume-button')
+const navigation = document.querySelector('nav')
+const scrollDownButton = document.querySelector('#scroll-down-button')
 
 window.addEventListener('load', () => {
-  pageLoadNameCover.style.transformOrigin = 'left';
+  pageLoadNameCover.style.transformOrigin = 'left'
   pageLoadNameCover.style.animation =
-    'grow-right 0.6s ease-in-out forwards 0.3s';
+    'grow-right 0.6s ease-in-out forwards 0.3s'
 
   setTimeout(() => {
-    pageLoadName.style.color = 'var(--color-primary)';
+    pageLoadName.style.color = 'var(--color-primary)'
 
-    pageLoadNameCover.style.transformOrigin = 'right';
-    pageLoadNameCover.style.animation =
-      'shrink-right 0.6s ease-in-out forwards';
-  }, 900);
-});
+    pageLoadNameCover.style.transformOrigin = 'right'
+    pageLoadNameCover.style.animation = 'shrink-right 0.6s ease-in-out forwards'
+  }, 900)
+})
 
 // ====================================================
 // scrolling down page events
 // ====================================================
-const navContainer = document.querySelector('#nav-container');
-const resumeBottomButton = document.querySelector('#resume-bottom-button');
+const navContainer = document.querySelector('#nav-container')
+const resumeBottomButton = document.querySelector('#resume-bottom-button')
 
 window.addEventListener('scroll', () => {
   // ====================================================
   // set navbar to solid color if not at top of page
   // ====================================================
   if (window.pageYOffset <= 0) {
-    navContainer.style.backgroundColor = 'transparent';
+    navContainer.style.backgroundColor = 'transparent'
   } else {
-    navContainer.style.backgroundColor = 'var(--color-secondary-light)';
+    navContainer.style.backgroundColor = 'var(--color-secondary-light)'
   }
 
   // ====================================================
@@ -70,44 +69,44 @@ window.addEventListener('scroll', () => {
   // ====================================================
   if (window.pageYOffset > window.innerHeight - 330) {
     if (resumeBottomButton.style.bottom != '4vw') {
-      resumeBottomButton.style.transition = 'bottom 0.5s ease-in-out';
-      resumeBottomButton.style.bottom = '4vw';
-      resumeBottomButton.style.right = '4vw';
+      resumeBottomButton.style.transition = 'bottom 0.5s ease-in-out'
+      resumeBottomButton.style.bottom = '4vw'
+      resumeBottomButton.style.right = '4vw'
     }
   } else {
     if (resumeBottomButton.style.right != '-90px') {
-      resumeBottomButton.style.transition = 'right 0.5s ease-in-out';
-      resumeBottomButton.style.right = '-90px';
+      resumeBottomButton.style.transition = 'right 0.5s ease-in-out'
+      resumeBottomButton.style.right = '-90px'
       setTimeout(() => {
-        resumeBottomButton.style.bottom = '-90px';
-      }, 500);
+        resumeBottomButton.style.bottom = '-90px'
+      }, 500)
     }
   }
-});
+})
 
 // ====================================================
 // focus animation for contact form input
 // ====================================================
-const inputUnderlineList = document.querySelectorAll('.inputUnderlineEffect');
-const inputList = document.querySelectorAll('.form-input');
+const inputUnderlineList = document.querySelectorAll('.inputUnderlineEffect')
+const inputList = document.querySelectorAll('.form-input')
 
 for (let i = 0; i < inputList.length; i++) {
   inputList[i].addEventListener('focus', () => {
-    inputUnderlineList[i].style.transform = 'scaleX(1)';
-  });
+    inputUnderlineList[i].style.transform = 'scaleX(1)'
+  })
 
   inputList[i].addEventListener('blur', () => {
-    inputUnderlineList[i].style.transform = 'scaleX(0)';
-  });
+    inputUnderlineList[i].style.transform = 'scaleX(0)'
+  })
 }
 
 // ====================================================
 // send user alert if input correct and submit pressed
 // ====================================================
-const contactSubmitButton = document.querySelector('#contact-submit-button');
+// const contactSubmitButton = document.querySelector('#contact-submit-button')
 
-contactSubmitButton.addEventListener('click', () => {
-  for (let i = 0; i < inputList.length; i++) if (!inputList[i].value) return;
+// contactSubmitButton.addEventListener('click', () => {
+//   for (let i = 0; i < inputList.length; i++) if (!inputList[i].value) return
 
-  alert('Your message has been sent!');
-});
+//   alert('Your message has been sent!')
+// })
